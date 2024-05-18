69bba0f180f1405f4906f1348ccd3ba44a0aeb22
Lasith Randima <lasithrandima123@gmail.com>
2024-05-18 22:02:12 +0530

    Laravel Mails And Notifications

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -3,9 +3,13 @@
 namespace App\Listeners;
 
 use App\Events\ClassCanceled;
+use App\Mail\ClassCanceledMail;
+use App\Notifications\ClassCanceledNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 
 class NotifyClassCanceled
 {
@@ -22,11 +26,27 @@ class NotifyClassCanceled
      */
     public function handle(ClassCanceled $event): void
     {
-        $scheduledClass = $event->scheduledClass;
-        Log::info($scheduledClass->classType->name . ' class canceled');
+        // $scheduledClass = $event->scheduledClass;
+        // Log::info($scheduledClass->classType->name . ' class canceled');
 
         // $scheduledClass->members->each(function ($member) use ($scheduledClass) {
         //     $member->notify(new ClassCanceledNotification($scheduledClass));
         // });
+
+        $members = $event->scheduledClass->members()->get();
+
+        $className = $event->scheduledClass->classType->name;
+        $classDateTime = $event->scheduledClass->date_time;
+
+        $details = compact('className', 'classDateTime');
+
+
+        // $members->each(function($user) use ($details){
+        //     Mail::to($user)->send(new ClassCanceledMail($details));
+        // });
+
+        //instead of sending mail to each member directly, we can send notification
+
+        Notification::send($members, new ClassCanceledNotification($details));
     }
 }
---
+++ resources/views/emails/class-canceled.blade.php
@@ -0,0 +1,2 @@
+<p>Hey, </p>
+<p> Sorry, Your {{ $details['className'] }} class on {{ $details['classDateTime']->format('jS F') }} at {{ $details['classDateTime']->format('g:i a') }} was canceled by the instructor. Check Schedule and Book Another. Thank You!</p>
